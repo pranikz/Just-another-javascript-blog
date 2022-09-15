75aa07c44ce5613069155720a9e3805e21fe8914
Pratyush <18btcse42@suiit.ac.in>
2022-09-15 16:46:54 +0530

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Layout from '../components/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout >
+      <Head>
+        <title>Javascript Blog by Pratyush</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
