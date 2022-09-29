b451eb6565b0e0b774df46fa4eef524c72c63d07
Pratyush <18btcse42@suiit.ac.in>
2022-09-29 19:48:33 +0530

    chapter publish with UI changes

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,30 @@
-import Image from "next/image";
 import Link from "next/link";
-import { GitIcon } from "../components/assets/icons";
+import Lottie from "react-lottie";
+import animationData from "../public/coding.json";
 
 export default function Home() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <div className="flex flex-wrap md items-center ">
+    <div className="flex flex-wrap ">
       <div className=" w-full md:w-1/2 ">
         <div className="mx-20 md:mx-32">
           <h1 className="text-4xl md:text-6xl font-bold mt-16">
             Just another <span className="text-[#eab308]">Javascript</span> Blog
           </h1>
-          {/* country region island */}
-          {/* description */}
           <div className="description w-full mt-12  text-gray-500 text-lg">
             Brushup all your javascript skills at one place. No googling around
             all infos required about your interviews at one place.
           </div>
           <div className="flex flex-col sm:flex-row my-12 font-light text-gray-500 mx-12 sm:mx-0">
             <div className="pr-4">
-              <button className="px-4 py-2 bg-[#eab308] border-black shadow-offset-black mt-5 hover:underline border-2 text-2xl text-gray-900 font-semibold pt-2">
+              <button className="px-4 py-2 bg-[#eab308]  border-black shadow-offset-black mt-5 hover:underline border-2 text-2xl text-gray-900 font-semibold pt-2">
                 <Link href="/jsblogs">Javascript</Link>
               </button>
             </div>
@@ -29,20 +35,23 @@ export default function Home() {
             </div>
             <div className="pr-4">
               <button className="px-12 py-2 bg-orange-300 border-black shadow-offset-black mt-5 hover:underline border-2 text-2xl text-gray-900 font-semibold pt-2">
-              <Link href="/dsablogs">DSA</Link>
+                <Link href="/dsablogs">DSA</Link>
               </button>
             </div>
           </div>
 
-          <div className="description w-full mt-12  text-gray-900 text-lg ">
+          {/* <div className="description w-full mt-12  text-gray-900 text-lg ">
             <div>Checkout our maintainers here!!</div>
             <div className="relative mx-11 sm:mx-0">
-              <button className=" px-4 py-2 text-black bg-orange-300 border-black shadow-offset-black uppercase mt-5 text-lg font-semibold hover:underline border-2 rounded ">
-                <Link href="/maintainers">maintainers</Link>
+              <button className=" px-1.5 py-2 text-black bg-orange-300 border-black shadow-offset-black  mt-5 text-2xl font-semibold hover:underline border-2  ">
+                <Link href="/maintainers">Maintainers</Link>
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
+      </div>
+      <div className="hidden md:inline-block w-full md:w-1/2">
+        <Lottie options={defaultOptions} height={600} width={500} />
       </div>
     </div>
   );
---
+++ posts/javascriptposts/chapter-2.0-Closures.md
@@ -0,0 +1,26 @@
+---
+title: "Chapter 2.0 Closures"
+metaTitle: "Closures "
+metaDesc: "Closures"
+socialImage: images/closures.png
+date: "2022-09-14"
+tags:
+  - javscript, nextjs
+---
+
+_We will be covering 3 topics in this module_
+
+# Chapter 2.1 - Closures
+
+1. Why learn about Closures?
+2. Scope
+3. What are Closures?
+4. Where is it used?
+
+# Chapter 2.2 - Closure Examples
+
+1. Examples (1-7)
+
+# Chapter 2.3 - Memoize
+
+1. Lodash.Memoize
