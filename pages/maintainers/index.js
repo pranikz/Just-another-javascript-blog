8bb4704f2528bc65dcf015f33c52c9cbb4b31615
Pratyush <18btcse42@suiit.ac.in>
2022-10-08 22:31:38 +0530

    fix: darkmode fix for maintainer page

--- pages/maintainers/index.js
+++ pages/maintainers/index.js
@@ -4,10 +4,10 @@ const index = () => {
       <section className="">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 rounded">
           <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-inherit ">
               Maintainers
             </h2>
-            <p className="font-medium text-gray-800 lg:mb-16 sm:text-xl ">
+            <p className="font-medium text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-500 ">
               Check out our Awesome Maintainers who help contribute in this blog
             </p>
           </div>
