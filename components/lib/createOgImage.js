fb391e0b6919c4915b91a360fd2a065c9320deea
Pratyush <18btcse42@suiit.ac.in>
2022-10-27 19:45:49 +0530

    feat: seo image added

--- components/lib/createOgImage.js
+++ components/lib/createOgImage.js
@@ -8,13 +8,13 @@ export const createOgImage = ({ title, meta }) =>
 
     // TITLE
     // Karla google font in light rose
-    `l_text:Karla_72_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
+    `l_text:Karla_75_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_180`,
 
     // META
     // Karla, but smaller
-    `l_text:Karla_48:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    `l_text:Karla_55:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
 
@@ -22,9 +22,9 @@ export const createOgImage = ({ title, meta }) =>
     // dynamically fetched from my twitter profile
     `l_twitter_name:pranikz`,
     // Transformations
-    `c_thumb,g_face,r_max,w_380,h_380,q_100`,
+    `c_thumb,g_face,r_max,w_500,h_500,q_100`,
     // Positioning
-    `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
+    `fl_layer_apply,w_190,g_north_west,x_100,y_100`,
 
     // BG
     `v1666878541/grain-gradient_co11sj.webp`,
